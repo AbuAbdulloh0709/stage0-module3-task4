8176ba404a16353a6c8fd642d929f21e95be07d0
Muslimbek Soliyev <abuabdulloh0709@gmail.com>
2023-01-09 19:02:29 +0500

    module 3 task 4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        double i = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2.f);
+
     }
 }
